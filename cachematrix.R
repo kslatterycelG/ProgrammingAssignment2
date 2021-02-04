3ff088d527d060e0c6761fe1b253ee82699a1489
Krystal Slattery <kslattery@celgene.com>
2021-02-04 15:36:30 -0800

    Programming Assignment 2: First Attempt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The goal is to have two functions, one that makes and caches an inverse
+## matrix and one that checks to see if there is a cached matrix before running
 
-## Write a short comment describing this function
+## This first function makes and caches the inverse matrix. I based the 
+## structure on the example provided for makeVector 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<- NULL
+  set<- function(y){
+    x <<- y
+    m <<- NULL 
+  }
+  get <- function() x
+  setInvm<- function() m <<- solve(x)
+  getInvm<- function() m
+  list(set = set, get = get, setInvm = setInvm, getInvm = getInvm)
 }
 
 
-## Write a short comment describing this function
+## This second function checks if there is a cached inverse matrix and 
+## returns the cached inverse or generates it 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<- x$getInvm()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+    m <- solve(data,...)
+    x$setInvm(m)
+    m
+          
 }
